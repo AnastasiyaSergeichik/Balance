b8e6f0a6211409952285084badc572f6a67ff038
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-09-15 15:01:47 +0300

    Добавление

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int balance = 100;
         int mindeposit = 1000;
         int refill = 1100;
@@ -7,18 +7,17 @@ public class Main {
         int charge = 1;
         int bonus;
         int allbalance;
-        if (refill>=mindeposit) {
+        if (refill >= mindeposit) {
             bonus = refill / spent * charge;
             allbalance = balance + refill + bonus;
-        }
-        else {
+        } else {
             bonus = 0;
             allbalance = balance + refill + bonus;
-                    }
+        }
         System.out.print("Бонус: ");
         System.out.println(bonus);
         System.out.print("Состояние счета: ");
         System.out.println(allbalance);
-                    }
+    }
 
 }
